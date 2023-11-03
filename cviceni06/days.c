c6b9b93b7fd6f3cbd1a9421a3582d357dc1e1c03
Viktor Jahodík <jahodvik@fit.cvut.cz>
2023-11-03 11:28:53 +0100

    CV06 Final working version 0.4p

--- cviceni06/days.c
+++ cviceni06/days.c
@@ -3,13 +3,76 @@
 #include <assert.h>
 #endif /* __PROGTEST__ */
 
+
+int leapYear(int a) {
+
+    if (a % 4000 == 0) {
+        return 0;
+    } else if (a % 400 == 0) {
+        return 1;
+    } else if (a % 100 == 0) {
+        return 0;
+    } else if (a % 4 == 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+
+
+}
+int daysInOneMonth(int y, int m) {
+
+    int monthDays[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    if (leapYear(y) == 1) { monthDays[1] = 29; }
+    return monthDays[m - 1];
+
+}
+int validateDate(int y, int m, int d) {
+    if (y < 2000) {
+
+        return 0;
+    }
+    if (m < 1 || m > 12) {
+        return 0;
+    }
+    if (d < 1) {
+        return 0;
+    }
+    if (d > daysInOneMonth(y, m)) {
+        return 0;
+    }
+    return 1;
+
+}
+
+
 int dateToIndex(int day, int month, int year, int *idx) {
     /* TODO: Your code here */
+*idx=0;
+    if (validateDate(year,month,day)==0){
+
+        return 0;
+    }
+    for (int i = 1; i < month; ++i) {
+        *idx+= daysInOneMonth(year,i);
+    }
+    *idx+=day;
+
+
+    return 1;
+
+
+
+
+
 }
 
 #ifndef __PROGTEST__
 int main (int argc, char * argv []) {
     int idx;
+
+
+
     assert(dateToIndex( 1,  1, 2000, &idx) == 1 && idx == 1);
     assert(dateToIndex( 1,  2, 2000, &idx) == 1 && idx == 32);
     assert(dateToIndex(29,  2, 2000, &idx) == 1 && idx == 60);
