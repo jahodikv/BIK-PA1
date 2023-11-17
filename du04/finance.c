443ad9d73d65c1e9ecb3f3be354ddc6dfc95cd82
Viktor Jahodík <jahodvik@fit.cvut.cz>
2023-11-17 15:13:14 +0100

    DU04: Input scanning is working

--- du04/finance.c
+++ du04/finance.c
@@ -1,21 +1,27 @@
 #include  <stdio.h>
 #include <stdlib.h>
+#define MAX_INPUT 25000
 
 
+int main(void) {
+    int balance[MAX_INPUT];
+    int x=0, i = 0;
+    while ( i< MAX_INPUT) {
 
+        x=scanf("%d", &balance[i]);
+        if(x==EOF){
+            break;
+        }
+        else if(x!=1){
+            printf("Nespravny vstup.\n");
+            return 0;
+        }
+        ++i;
 
+    }
 
-int main(void){
-
-
-
-
-
-
-
-
-
-
+    printf("%d",balance[0]);
     return 0;
+
 
 }
