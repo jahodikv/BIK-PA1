1dcc54fd92e6699a6b32b39a4756b35dda5f24c1
Viktor Jahodík <jahodvik@fit.cvut.cz>
2023-12-22 17:25:25 +0100

    DU08: Added free, create, add list

--- du08/test.c
+++ du08/test.c
@@ -18,20 +18,100 @@ typedef struct TCriminal
 TCRIMINAL        * createRecord ( const char      * name,
                                   TCRIMINAL       * next )
 {
-  /* todo */
-}
-TCRIMINAL        * cloneList    ( TCRIMINAL       * src )
-{
-  /* todo */
+    TCRIMINAL *l = (TCRIMINAL *)malloc(sizeof(TCRIMINAL));
+    l->m_Name = (char *)malloc(strlen(name) + 1);
+    strcpy(l->m_Name, name);
+    l->m_Contacts = NULL;
+    l->m_Cnt = 0;
+    l->m_Capacity = 0;
+
+    l->m_Next = next;
+    return l;
+
+
+
+
+
 }
 void               addContact   ( TCRIMINAL       * dst,
                                   TCRIMINAL       * contact )
 {
-  /* todo */
+
+    if (dst->m_Cnt == dst->m_Capacity) {
+
+        size_t newCapacity = (dst->m_Capacity == 0) ? 1 : dst->m_Capacity * 2;
+
+        TCRIMINAL **newContacts = (TCRIMINAL **)realloc(dst->m_Contacts, newCapacity * sizeof(TCRIMINAL *));
+
+
+        dst->m_Contacts = newContacts;
+        dst->m_Capacity = newCapacity;
+    }
+
+    dst->m_Contacts[dst->m_Cnt++] = contact;
 }
+TCRIMINAL        * cloneList    ( TCRIMINAL       * src ) {
+    if (src == NULL) {
+        return NULL;
+    } else {
+
+        TCRIMINAL *newNode = (TCRIMINAL *)malloc(sizeof(TCRIMINAL));
+
+        newNode->m_Contacts = (TCRIMINAL **)malloc(newNode->m_Capacity * sizeof(TCRIMINAL *));
+        for (size_t i = 0; i < newNode->m_Cnt; ++i) {
+            newNode->m_Contacts[i] = NULL; // Initialize each contact pointer to NULL
+
+            TCRIMINAL *currentContact = src->m_Contacts[i];
+            TCRIMINAL *previousClonedContact = NULL;
+
+            while (currentContact != NULL) {
+                TCRIMINAL *clonedContact = (TCRIMINAL *)malloc(sizeof(TCRIMINAL));
+
+                clonedContact->m_Name = (char *)malloc(strlen(currentContact->m_Name) + 1);
+                strcpy(clonedContact->m_Name, currentContact->m_Name);
+
+                // Link the cloned contact to the cloned list
+                if (previousClonedContact == NULL) {
+                    newNode->m_Contacts[i] = clonedContact;
+                } else {
+                    previousClonedContact->m_Next = clonedContact;
+                }
+                previousClonedContact = clonedContact;
+
+                currentContact = currentContact->m_Next;
+            }
+        }
+
+                newNode->m_Name = (char *)malloc(strlen(src->m_Name) + 1);
+        strcpy(newNode->m_Name, src->m_Name);
+        newNode->m_Cnt = src->m_Cnt;
+        newNode->m_Capacity = src->m_Capacity;
+
+        newNode->m_Next = cloneList(src->m_Next);
+
+        return newNode;
+    }
+}
+
+
 void               freeList     ( TCRIMINAL       * src )
 {
-  /* todo */
+    TCRIMINAL *current = src;
+    TCRIMINAL *next;
+    while (current != NULL) {
+
+        free(current->m_Name);
+        free(current->m_Contacts);
+
+
+        next = current->m_Next;
+
+
+        free(current);
+
+
+        current = next;
+    }
 }
 
 #ifndef __PROGTEST__
@@ -41,7 +121,7 @@ int main ( int argc, char * argv [] )
   char tmp[100];
 
   assert ( sizeof ( TCRIMINAL ) == 3 * sizeof ( void * ) + 2 * sizeof ( size_t ) );
-  a = nullptr;
+  a = NULL;
   a = createRecord ( "Peter", a );
   a = createRecord ( "John", a );
   a = createRecord ( "Joe", a );
@@ -64,7 +144,7 @@ int main ( int argc, char * argv [] )
            && ! strcmp ( a -> m_Next -> m_Next -> m_Next -> m_Name, "Peter" )
            && a -> m_Next -> m_Next -> m_Next -> m_Cnt == 1
            && a -> m_Next -> m_Next -> m_Next -> m_Contacts[0] == a -> m_Next );
-  assert ( a -> m_Next -> m_Next -> m_Next -> m_Next == nullptr );
+  assert ( a -> m_Next -> m_Next -> m_Next -> m_Next == NULL);
   b = cloneList ( a );
   strcpy ( tmp, "Moe" );
   a = createRecord ( tmp, a );
@@ -97,7 +177,7 @@ int main ( int argc, char * argv [] )
            && ! strcmp ( a -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Name, "Peter" )
            && a -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Cnt == 1
            && a -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Contacts[0] == a -> m_Next -> m_Next -> m_Next -> m_Next );
-  assert ( a -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next == nullptr );
+  assert ( a -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next == NULL );
   assert ( b
            && ! strcmp ( b -> m_Name, "Maria" )
            && b -> m_Cnt == 1
@@ -114,7 +194,7 @@ int main ( int argc, char * argv [] )
            && b -> m_Next -> m_Next -> m_Next -> m_Cnt == 2
            && b -> m_Next -> m_Next -> m_Next -> m_Contacts[0] == b -> m_Next
            && b -> m_Next -> m_Next -> m_Next -> m_Contacts[1] == b -> m_Next -> m_Next );
-  assert ( b -> m_Next -> m_Next -> m_Next -> m_Next == nullptr );
+  assert ( b -> m_Next -> m_Next -> m_Next -> m_Next == NULL );
   freeList ( a );
   addContact ( b -> m_Next, b -> m_Next );
   a = cloneList ( b );
@@ -135,7 +215,7 @@ int main ( int argc, char * argv [] )
            && a -> m_Next -> m_Next -> m_Next -> m_Cnt == 2
            && a -> m_Next -> m_Next -> m_Next -> m_Contacts[0] == a -> m_Next
            && a -> m_Next -> m_Next -> m_Next -> m_Contacts[1] == a -> m_Next -> m_Next );
-  assert ( a -> m_Next -> m_Next -> m_Next -> m_Next == nullptr );
+  assert ( a -> m_Next -> m_Next -> m_Next -> m_Next == NULL );
   assert ( b
            && ! strcmp ( b -> m_Name, "Maria" )
            && b -> m_Cnt == 1
@@ -153,7 +233,7 @@ int main ( int argc, char * argv [] )
            && b -> m_Next -> m_Next -> m_Next -> m_Cnt == 2
            && b -> m_Next -> m_Next -> m_Next -> m_Contacts[0] == b -> m_Next
            && b -> m_Next -> m_Next -> m_Next -> m_Contacts[1] == b -> m_Next -> m_Next );
-  assert ( b -> m_Next -> m_Next -> m_Next -> m_Next == nullptr );
+  assert ( b -> m_Next -> m_Next -> m_Next -> m_Next == NULL );
   freeList ( b );
   freeList ( a );
   return EXIT_SUCCESS;
